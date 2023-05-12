fbb1997797edb890ab32291f0c58aedff8127ccd
SeikaYoruno <misuzu14pc@gmail.com>
2023-05-12 17:46:45 +0900

    Update Underside.ino

--- Underside/Underside.ino
+++ Underside/Underside.ino
@@ -1,38 +1,48 @@
-// # Editor     : roker
-// # Date       : 05.03.2018
+#include <TimerTCC0.h>
+#include <Adafruit_DPS310.h>
 
-// # Product name: URM V5.0 ultrasonic sensor
-// # Product SKU : SEN0001
-// # Version     : 1.0
+Adafruit_DPS310 dps;
 
-// # Description:
-// # The Sketch for scanning 180 degree area 2-800cm detecting range
-// # The sketch for using the URM37 PWM trigger pin mode from DFRobot
-// #   and writes the values to the serialport
-// # Connection:
-// #       Vcc (Arduino)    -> Pin 1 VCC (URM V5.0)
-// #       GND (Arduino)    -> Pin 2 GND (URM V5.0)
-// #       Pin 3 (Arduino)  -> Pin 4 ECHO (URM V5.0)
-// #       Pin 5 (Arduino)  -> Pin 6 COMP/TRIG (URM V5.0)
+#define DPS310_CS 10
 
-// # Working Mode: PWM trigger pin  mode.
-
-int URECHO = 10;  // PWM Output 0-50000US,Every 50US represent 1cm
-int URTRIG = 9;   // trigger pin
+int URECHO = 0;  // PWM Output 0-50000US,Every 50US represent 1cm
+int URTRIG = 1;   // trigger pin
 
 unsigned int DistanceMeasured = 0;
 
+float pressure_hPa = 0;
+float temperature_deg = 0;
+float altitude_pressure_m = 0;
+float altitude_ultrasonic_m = 0;
+
+char Serial_BUF[256];
+
 void setup() {
   //Serial initialization
-  Serial.begin(9600);          // Sets the baud rate to 9600
+  Serial.begin(115200);          // Sets the baud rate to 9600
   pinMode(URTRIG, OUTPUT);     // A low pull on pin COMP/TRIG
   digitalWrite(URTRIG, HIGH);  // Set to HIGH
   pinMode(URECHO, INPUT);      // Sending Enable PWM mode command
   delay(500);
   Serial.println("Init the sensor");
+
+  Nire.setClock(400000);
+  Serial.println("DPS310");
+  if (! dps.begin_I2C()) {             // Can pass in I2C address here
+    //if (! dps.begin_SPI(DPS310_CS)) {  // If you want to use SPI
+    Serial.println("Failed to find DPS");
+    while (1) yield();
+  }
+  Serial.println("DPS OK!");
+
+  dps.configurePressure(DPS31032HZ, DPS310_16SAMPLES);
+  dps.configureTemperature(DPS310_32HZ, DPS310_2SAMPLES);
+
+  TimerTcc0.initialize(10000);
+  TimerTcc0.attachInterrupt(timerIsr);
 }
 void loop() {
-  Serial.print("Distance=");
+  //Serial.print("Distance=");
   digitalWrite(URTRIG, LOW);
   delay(1);
   digitalWrite(URTRIG, HIGH);
@@ -40,12 +50,27 @@ void loop() {
   unsigned long LowLevelTime = pulseIn(URECHO, LOW);
   if (LowLevelTime >= 50000)  // the reading is invalid.
   {
-    Serial.println("Invalid");
+    //Serial.println("Invalid");
   } else {
     DistanceMeasured = LowLevelTime / 50;  // every 50us low level stands for 1cm
-    Serial.print(DistanceMeasured);
-    Serial.println("cm");
+    altitude_ultrasonic_m = (float)DistanceMeasured / 100.0;
   }
 
   //delay(200);
+
+
+}
+void timerIsr()
+{
+
+  sensors_event_t temp_event, pressure_event;
+
+  if (dps.temperatureAvailable() && dps.pressureAvailable()) {
+    dps.getEvents(&temp_event, &pressure_event);
+    temperature_deg = temp_event.temperature;
+    pressure_hPa = pressure_event.pressure; 
+  }
+
+  sprintf(Serial_BUF, "%f,%f,%f,%f\n" , pressure_hPa, temperature_deg , altitude_pressure_m , altitude_ultrasonic_m);
+  Serial.print(Serial_BUF);
 }
